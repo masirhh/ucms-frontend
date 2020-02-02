c1dff3bac07b1e653dbf7f623794e99b29528f36
masirhh <masirhh@outlook.com>
2020-02-02 18:08:44 +0800

    [add] add user api

---
+++ ucms-frontend/src/components/User.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2>This a User Page</h2>
+  </div>
+</template>
+
+<script>
+import {reqUser} from "../network";
+
+export default {
+  name: "user",
+  data() {
+    return {
+      users: ""
+    };
+  }
+};
+
+reqUser().then(res=>console.log(res.data))
+</script>
+
+<style>
+</style>
--- ucms-frontend/src/network/index.js
+++ ucms-frontend/src/network/index.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
+
+
 export function reqUser(config) {
     const instance = axios.create({
-        baseURL: 'http://localhost:8081/user',
+        baseURL: 'http://localhost:8081/ucms/user',
         timeout: 5000
     })
     return instance(config)
